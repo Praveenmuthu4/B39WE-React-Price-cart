0483c0e2e4e6c43af937c427fbfb59c50d141eef
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-06-06 21:19:13 +0530

    Created Price card using react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CardData1 from "./component/CardData1";
+import CardData2 from "./component/CardData2";
+import CardData3 from "./component/CardData3";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="pricing py-5">
+      <div className="container">
+        <div className="row">
+          <CardData1 />
+          <CardData2 />
+          <CardData3 />
+        </div>
+      </div>
+    </section>
   );
 }
 
